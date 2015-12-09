d650e342d9537a0dd2b57695bba0a01a2e45c9a6
Siddharth Murali <sm1254@scarletmail.rutgers.edu>
2015-12-09 00:49:26 -0500

    initial defines and stuff

---
+++ utils.h
@@ -0,0 +1,12 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <pthread.h>
+
+
